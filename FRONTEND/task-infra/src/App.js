24f29cb29f1a06d1483588de38a505826bda7d55
ADITHYAKUMARREDDY <91198686+ADITHYAKUMARREDDY@users.noreply.github.com>
2024-07-28 22:55:17 +0530

    new changes

--- FRONTEND/task-infra/src/App.js
+++ FRONTEND/task-infra/src/App.js
@@ -2,9 +2,14 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./components/Login";
 import Data from "./components/Data";
+
+
+
 export const config = {
   endpoint: `http://localhost:5002`,
 };
+
+
 function App() {
   const [userName, setUserName] = useState(""); // Manage userName state
 
